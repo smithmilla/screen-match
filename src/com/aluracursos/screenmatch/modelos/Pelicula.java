d433970fbb93715a5bc954c16470e54df6f2e715
iSmithmilla <jhonmilla1607@gmail.com>
2025-04-15 18:01:46 -0500

    2025.04.15 - feat: creacion clases

---
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -0,0 +1,31 @@
+public class Pelicula {
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
+
+    int getTotalDeLasEvaluaciones(){
+        return totalDeLasEvaluaciones;
+    }
+
+    double getSumaDeLasEvaluaciones(){
+        return sumaDeLasEvaluaciones;
+    }
+
+    void muestraFichaTecnica(){
+        System.out.println("Nombre de la pelicula: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Duracion en minutos: " + duracionEnMinutos);
+    }
+
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
+}
